3dbf80c118b754c77ae5f85cdb89e4d037a27207
chocosod <heavensblue@hanmail.net>
2024-08-08 14:05:31 +0900

    8.08

--- js/script.js
+++ js/script.js
@@ -2,3 +2,17 @@ var sgc1 = new ScrollGiveClass("header",{
     baseline:'100'
     //scroll막대가 100px내려오면 클래스 부여 
 });
+
+//Swiper플러그인 호출
+
+const swiper1 = new Swiper('#main_view', {
+    // Optional parameters
+    loop: false,
+    wrapperClass:'slider',
+    slideClass:'view',
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },  
+  });
